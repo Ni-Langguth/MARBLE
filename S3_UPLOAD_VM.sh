903d90d4fabbb5d1d02f077e194a31dc2170b5ac
Ni-Langguth <nila1002@stud.hs-kl.de>
2025-09-15 15:44:17 +0200

    Added interactive arguments, the script now: resets zsh_history, removes PAT before uploading automatically, renames VM internally and deletes snapshots before uploading

--- S3_UPLOAD_VM.sh
+++ S3_UPLOAD_VM.sh
@@ -1,35 +1,56 @@
-CURRENT_VM_NAME=$1
+if [ -z "$1" ]; then
+  read -p "Enter the name of the VM you want to upload: " CURRENT_VM_NAME
+else
+  CURRENT_VM_NAME=$1
+fi
 HOST_NAME=$(scutil --get ComputerName)
-STORAGE_VM_NAME="${CURRENT_VM_NAME#${HOST_NAME}-}"
-TEAM=$(echo "$STORAGE_VM_NAME" | cut -d'-' -f1)
+PURE_VM_NAME="${CURRENT_VM_NAME#${HOST_NAME}-}"
+STORAGE_VM_NAME="$(date +%F)-$PURE_VM_NAME"
+TEAM=$(echo "$PURE_VM_NAME" | cut -d'-' -f1)
 USER_DIR=/Users/sftnight
 PARALLELS_DIR=$USER_DIR/Parallels
 S3_BUCKET=s3://macvmstorage
 
-
-
-# CLEAN SECRETS FROM VM BEFORE UPLOADING:
-# REMOVE PAT from VM
-prlctl start $1
-sleep 5
-prlctl exec $1 'sudo rm /Users/sftnight/.PAT'
-sleep 1
-prlctl stop $CURRENT_VM_NAME --kill
-sleep 5
-
-prlctl clone "$CURRENT_VM_NAME" --name "$STORAGE_VM_NAME"
-SNAPSHOTS=$(prlctl snapshot-list "$STORAGE_VM_NAME" -t | tr -d '*' | tr -d '{}' | tr ' ' '\n' | grep -v '^$')
-if [[ -z "$SNAPSHOTS" ]]; then
-  echo "No snapshots found."
-else
-  echo "Found snapshots:"
-  echo "$SNAPSHOTS"
-
-  for SNAP in $SNAPSHOTS; do
-    echo "Deleting snapshot $SNAP..."
-    prlctl snapshot-delete "$VM_NAME" -i "$SNAP"
-  done
+if [[ $(prlctl list --no-header | wc -l) -ne 0 ]]; then
+  echo "Make sure that all VMs on this machine are stopped first."
+  exit
 fi
+
+# Clone VM - script leaves the original as is
+prlctl clone "$CURRENT_VM_NAME" --name "$STORAGE_VM_NAME"
+
+# REMOVE PAT
+prlctl start $STORAGE_VM_NAME
+sleep 15
+prlctl exec $STORAGE_VM_NAME "sudo rm /Users/sftnight/.PAT"
+# CLEAR ZSH_HISTORY
+prlctl exec $STORAGE_VM_NAME "rm /Users/sftnight/.zsh_history && touch /Users/sftnight/.zsh_history && chmod 600 /Users/sftnight/.zsh_history"
+# RENAME VM
+prlctl exec $STORAGE_VM_NAME "sudo scutil --set ComputerName $STORAGE_VM_NAME"
+prlctl exec $STORAGE_VM_NAME "sudo scutil --set HostName $STORAGE_VM_NAME"
+sleep 1
+prlctl stop $STORAGE_VM_NAME --kill
+sleep 3
+
+SNAPSHOTS=$(prlctl snapshot-list "$STORAGE_VM_NAME" -t | tr -d '*' | tr -d '{}' | tr ' ' '\n' | grep -v '^$')
+echo $SNAPSHOTS
+while [[ ! -z "$SNAPSHOTS" ]]; do
+  if [[ -z "$SNAPSHOTS" ]]; then
+    echo "No snapshots found."; break
+  else
+    echo "Found snapshots:"
+    echo "$SNAPSHOTS"
+
+    for SNAP in $SNAPSHOTS; do
+      echo "Deleting snapshot $SNAP from VM $STORAGE_VM_NAME"
+      prlctl snapshot-delete "$STORAGE_VM_NAME" -i "$SNAP"
+    done
+    sleep 1
+    SNAPSHOTS=$(prlctl snapshot-list "$STORAGE_VM_NAME" -t | tr -d '*' | tr -d '{}' | tr ' ' '\n' | grep -v '^$')
+  fi
+done
+
+# Zip clone
 zip -r /Users/sftnight/Parallels/"$STORAGE_VM_NAME".zip /Users/sftnight/Parallels/"$STORAGE_VM_NAME".macvm
 
 prlctl unregister $STORAGE_VM_NAME
