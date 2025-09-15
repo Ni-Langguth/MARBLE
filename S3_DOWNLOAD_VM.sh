d7dde8bc520db779b849136b0763eab16d778ef8
Ni-Langguth <nila1002@stud.hs-kl.de>
2025-09-15 17:39:35 +0200

    Adjusted instructions after download to accommodate reinstall of parallels tools

--- S3_DOWNLOAD_VM.sh
+++ S3_DOWNLOAD_VM.sh
@@ -40,22 +40,19 @@ else
   rm -rf /Users/sftnight/Parallels/build_drive.hdd.zip
 fi
 
-# RENAME VM INTERNALLY
-
-prlctl start $VM_NAME
-sleep 15
-prlctl exec $VM_NAME "sudo scutil --set ComputerName $VM_NAME"
-prlctl exec $VM_NAME "sudo scutil --set HostName $VM_NAME"
-prlctl stop $VM_NAME --kill
-sleep 3
 # CLEAN UP VM ZIP
 rm -rf /Users/sftnight/Parallels/${STORAGE_VM_NAME}.zip
+
 MSG="
 After executing this script, the following steps must be performed manually: \n
 \t - Register the VM to the CERN network at https://landb.cern.ch/portal/devices/register \n
 \t\t  - mac-address: $(prlctl list -i ${VM_NAME} | grep net | sed -n 's/.*mac=\([^ ]*\).*/\1/p') \n
 \t\t  - name: ${VM_NAME} \n
-\t\t  - opt out of IPV6, it can lead to problems
-\t - If you want to set this VM up as a CI machine, follow instructions in README.md, specifically 'Create a CI VM', 'Add to CI'
+\t\t  - opt out of IPV6, it can lead to problems \n
+\t\t  - log into the VMs GUI once, the reinstallation of Parallels Tools will start automatically after a moment, then require a restart \n
+\t\t  - once the VM is restarted, execute these two lines on the host: \n
+prlctl exec $VM_NAME "sudo scutil --set ComputerName $VM_NAME"
+prlctl exec $VM_NAME "sudo scutil --set HostName $VM_NAME"
+\t - Finally, if you want to set this VM up as a CI machine, look up README>Create a CI VM>Then add the VM to CI
 "
 echo $MSG
