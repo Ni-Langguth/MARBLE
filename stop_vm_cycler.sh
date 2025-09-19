8ad254f43690b865109bdce64730bf082f189abe
Ni-Langguth <nila1002@stud.hs-kl.de>
2025-09-19 20:14:13 +0200

    add kill -9 to stop_vm_cycler.sh

--- stop_vm_cycler.sh
+++ stop_vm_cycler.sh
@@ -3,7 +3,7 @@ SCRIPT=vm_cycler.sh
 
 if [[ $(pgrep -f ${SCRIPT}) ]]; then
   echo "killing ${SCRIPT}"
-  kill $(pgrep -f ${SCRIPT})
+  kill -9 $(pgrep -f ${SCRIPT})
 else
   echo "${SCRIPT} not found"
 fi
